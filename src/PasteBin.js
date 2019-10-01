d980b4f64fcfccaeb6aea528e691de97847a2674
kidman90 <kacprzak90@gmail.com>
2019-10-01 11:55:53 +0200

    PasteBin and Button components added

---
+++ src/PasteBin.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function PasteBin(props) {
+  return (
+    <textarea
+      style={{
+        width: '100%',
+        margin: '12px 0',
+        outline: 'none',
+        padding: 12,
+        border: '2px solid #eee',
+        color: '#666',
+        borderRadius: 4,
+      }}
+      rows={25}
+      {...props}
+    />
+  );
+}
+
+export default PasteBin;
